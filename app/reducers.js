cbbfc5503a3fc6c14c994a1f3d8c2cb14825168f
bhaveshpawar07 <bhavesh.pawar@wednesday.is>
2022-08-18 17:26:59 +0530

    fix: remove form component

--- app/reducers.js
+++ app/reducers.js
@@ -5,7 +5,6 @@
 import { combineReducers } from 'redux';
 import languageProviderReducer from 'containers/LanguageProvider/reducer';
 import homeContainerReducer from 'containers/HomeContainer/reducer';
-import formReducer from './containers/Form/reducer';
 import iTunesReducer from './containers/ITunes/reducer';
 
 /*** Merges the main reducer with the router state and dynamically injected reducers*/
@@ -14,7 +13,6 @@ export default function createReducer(injectedReducer = {}) {
     ...injectedReducer,
     language: languageProviderReducer,
     homeContainer: homeContainerReducer,
-    form: formReducer,
     iTunes: iTunesReducer
   });
 
