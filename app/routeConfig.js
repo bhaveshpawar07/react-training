813d2462442a8d0bb885cbf3ca800343fcb343b1
bhaveshpawar07 <bhavesh.pawar@wednesday.is>
2022-08-18 13:04:34 +0530

    feat: Render iTunes on '/'

--- app/routeConfig.js
+++ app/routeConfig.js
@@ -2,7 +2,12 @@ import NotFound from '@containers/NotFoundPage/Loadable';
 import HomeContainer from '@containers/HomeContainer/Loadable';
 import routeConstants from '@utils/routeConstants';
 import Form from '@containers/Form';
+import ITunes from '@containers/ITunes/Loadable';
 export const routeConfig = {
+  iTunes: {
+    component: ITunes,
+    ...routeConstants.iTunes
+  },
   repos: {
     component: HomeContainer,
     ...routeConstants.repos
--- app/utils/routeConstants.js
+++ app/utils/routeConstants.js
@@ -1,5 +1,13 @@
 export default {
   repos: {
+    route: '/repos',
+    props: {
+      maxwidth: 500,
+      padding: 20
+    },
+    exact: true
+  },
+  iTunes: {
     route: '/',
     props: {
       maxwidth: 500,
