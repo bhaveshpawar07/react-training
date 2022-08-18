93cae327719c50f694eb886d1127187df88bf74e
bhaveshpawar07 <bhavesh.pawar@wednesday.is>
2022-08-18 16:18:58 +0530

    fix: Remove form component

--- app/reducers.js
+++ app/reducers.js
@@ -5,15 +5,13 @@
 import { combineReducers } from 'redux';
 import languageProviderReducer from 'containers/LanguageProvider/reducer';
 import homeContainerReducer from 'containers/HomeContainer/reducer';
-import formReducer from './containers/Form/reducer';
 
 /*** Merges the main reducer with the router state and dynamically injected reducers*/
 export default function createReducer(injectedReducer = {}) {
   const rootReducer = combineReducers({
     ...injectedReducer,
     language: languageProviderReducer,
-    homeContainer: homeContainerReducer,
-    form: formReducer
+    homeContainer: homeContainerReducer
   });
 
   return rootReducer;
--- app/routeConfig.js
+++ app/routeConfig.js
@@ -1,7 +1,6 @@
 import NotFound from '@containers/NotFoundPage/Loadable';
 import HomeContainer from '@containers/HomeContainer/Loadable';
 import routeConstants from '@utils/routeConstants';
-import Form from '@containers/Form';
 import ITunes from '@containers/ITunes/Loadable';
 export const routeConfig = {
   iTunes: {
@@ -11,10 +10,6 @@ export const routeConfig = {
   repos: {
     component: HomeContainer,
     ...routeConstants.repos
-  },
-  form: {
-    component: Form,
-    ...routeConstants.form
   },
   notFoundPage: {
     component: NotFound,
