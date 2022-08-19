e1943eb45b1eea938371a77541fdebac6811372f
bhaveshpawar07 <bhavesh.pawar@wednesday.is>
2022-08-19 13:55:57 +0530

    fix : coverage for all functions

--- app/containers/ITunes/tests/reducer.test.js
+++ app/containers/ITunes/tests/reducer.test.js
@@ -5,12 +5,33 @@ describe('ITunes reducer tests', () => {
     expect(iTunesReducer(undefined, {})).toEqual(initialState);
   });
 
-  it('should return the updated state when an action of type DEFAULT is dispatched', () => {
+  it('should return the updated state when an action of type REQUEST_GET_ITUNES_DATA is dispatched', () => {
     const expectedResult = { ...initialState, tuneName: 'test' };
     expect(
       iTunesReducer(initialState, {
         type: iTunesTypes.REQUEST_GET_ITUNES_DATA,
         tuneName: 'test'
+      })
+    ).toEqual(expectedResult);
+  });
+  it('should return the itunes data when an action of type SUCCESS_GET_ITUNES_DATA is dispatched', () => {
+    const data = { resultCount: 1, result: [{ tuneName: 'test' }] };
+    const expectedResult = { ...initialState, tuneData: data };
+    expect(
+      iTunesReducer(initialState, {
+        type: iTunesTypes.SUCCESS_GET_ITUNES_DATA,
+        data
+      })
+    ).toEqual(expectedResult);
+  });
+
+  it('should return the error when an action of type FAILURE_GET_ITUNES_DATA is dispatched', () => {
+    const error = 'something_went_wrong';
+    const expectedResult = { ...initialState, tuneError: error };
+    expect(
+      iTunesReducer(initialState, {
+        type: iTunesTypes.FAILURE_GET_ITUNES_DATA,
+        tuneError: error
       })
     ).toEqual(expectedResult);
   });
